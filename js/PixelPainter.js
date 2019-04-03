2813319570d9e82e4a546ddb7642d853511c2f26
Brett <brett_matsumoto@yahoo.com>
2019-04-03 12:14:36 -1000

    there was a better way

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -49,42 +49,11 @@ const makeColorSwath = function (x, y) {
 }
 makeColorSwath(4, 4);
 
-//I know there is a better way to do this somewhere but i'd rather just get pixels on screen instead of staring into the abyss trying to figure this out
+const getColorClass = document.getElementsByClassName('colorBoxes');
 
-const blkColor = document.getElementById('color1');
-blkColor.addEventListener('click', getColor);
-const bluColor = document.getElementById('color2');
-bluColor.addEventListener('click', getColor);
-const purpColor = document.getElementById('color3');
-purpColor.addEventListener('click', getColor);
-const tealColor = document.getElementById('color4');
-tealColor.addEventListener('click', getColor);
-const redColor = document.getElementById('color5');
-redColor.addEventListener('click', getColor);
-const cfbColor = document.getElementById('color6');
-cfbColor.addEventListener('click', getColor);
-const magenColor = document.getElementById('color7');
-magenColor.addEventListener('click', getColor);
-const salmColor = document.getElementById('color8');
-salmColor.addEventListener('click', getColor);
-const goldColor = document.getElementById('color9');
-goldColor.addEventListener('click', getColor);
-const pinkColor = document.getElementById('color10');
-pinkColor.addEventListener('click', getColor);
-const limeColor = document.getElementById('color11');
-limeColor.addEventListener('click', getColor);
-const sadBColor = document.getElementById('color12');
-sadBColor.addEventListener('click', getColor);
-const slaGrayColor = document.getElementById('color13');
-slaGrayColor.addEventListener('click', getColor);
-const khakiColor = document.getElementById('color14');
-khakiColor.addEventListener('click',getColor);
-const sandBroColor = document.getElementById('color15');
-sandBroColor.addEventListener('click', getColor);
-const killMeNow = document.getElementById('color16');
-killMeNow.addEventListener('click', getColor);
-
-
+for ( var i = 0; i < getColorClass.length; i++){
+  getColorClass[i].addEventListener('click', getColor)
+}
 
 function getColor() {
   console.log('test')
