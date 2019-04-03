88a8b3849e6e69640d7e22be83dfa5ddcf3be5e2
Brett <brett_matsumoto@yahoo.com>
2019-04-03 12:05:35 -1000

    added events to each color the long way

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,5 +1,11 @@
+//Area to make HTML
+
+//
+
+
 const colorArea = document.getElementById('colors');
 const canvasArea = document.getElementById('canvas');
+
 
 const makeCanvasMatrix = function (x, y) {
   let colCount = 1;
@@ -7,7 +13,7 @@ const makeCanvasMatrix = function (x, y) {
 
   for (let i = 0; i < x; i++) {
     let collum = document.createElement('div');
-    collum.id = 'col' + colCount;
+    collum.id = colCount;
     collum.className = 'canvasCollumns'
     canvasArea.appendChild(collum);
     colCount++;
@@ -20,7 +26,7 @@ const makeCanvasMatrix = function (x, y) {
     }
   }
 }
-makeCanvasMatrix(40, 40);
+makeCanvasMatrix(50, 50);
 
 const makeColorSwath = function (x, y) {
   let colorCollumCount = 1;
@@ -33,22 +39,53 @@ const makeColorSwath = function (x, y) {
     colorCollumCount++;
     for (let a = 0; a < y; a++) {
       let colorBox = document.createElement('div');
-      colorBox.id = colorBoxCount;
+      colorBox.id = 'color' + colorBoxCount;
       colorBox.className = 'colorBoxes';
       colorBoxCount++;
-      if (colorBox.id < 5) {
-        colorBox1.appendChild(colorBox);
-      }
-      if (colorBox.id > 4 && colorBox.id < 9) {
-        colorBox2.appendChild(colorBox);
-      }
-      if (colorBox.id > 8 && colorBox.id < 13) {
-        colorBox3.appendChild(colorBox);
-      }
-      if (colorBox.id > 12 && colorBox.id < 17) {
-        colorBox4.appendChild(colorBox);
-      }
+      colorCollum.appendChild(colorBox);
+
     }
   }
 }
-makeColorSwath(4,4);
+makeColorSwath(4, 4);
+
+//I know there is a better way to do this somewhere but i'd rather just get pixels on screen instead of staring into the abyss trying to figure this out
+
+const blkColor = document.getElementById('color1');
+blkColor.addEventListener('click', getColor);
+const bluColor = document.getElementById('color2');
+bluColor.addEventListener('click', getColor);
+const purpColor = document.getElementById('color3');
+purpColor.addEventListener('click', getColor);
+const tealColor = document.getElementById('color4');
+tealColor.addEventListener('click', getColor);
+const redColor = document.getElementById('color5');
+redColor.addEventListener('click', getColor);
+const cfbColor = document.getElementById('color6');
+cfbColor.addEventListener('click', getColor);
+const magenColor = document.getElementById('color7');
+magenColor.addEventListener('click', getColor);
+const salmColor = document.getElementById('color8');
+salmColor.addEventListener('click', getColor);
+const goldColor = document.getElementById('color9');
+goldColor.addEventListener('click', getColor);
+const pinkColor = document.getElementById('color10');
+pinkColor.addEventListener('click', getColor);
+const limeColor = document.getElementById('color11');
+limeColor.addEventListener('click', getColor);
+const sadBColor = document.getElementById('color12');
+sadBColor.addEventListener('click', getColor);
+const slaGrayColor = document.getElementById('color13');
+slaGrayColor.addEventListener('click', getColor);
+const khakiColor = document.getElementById('color14');
+khakiColor.addEventListener('click',getColor);
+const sandBroColor = document.getElementById('color15');
+sandBroColor.addEventListener('click', getColor);
+const killMeNow = document.getElementById('color16');
+killMeNow.addEventListener('click', getColor);
+
+
+
+function getColor() {
+  console.log('test')
+}
